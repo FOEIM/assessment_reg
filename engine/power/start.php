f2463d2318984ebd501714d0a4c09855e82e5885
FOEIM <87746026+FOEIM@users.noreply.github.com>
2021-07-21 12:08:03 +0630

    Create start.php

---
+++ engine/power/start.php
@@ -0,0 +1,25 @@
+<?php
+/*$mysqli = new mysqli("localhost","foeimorg","foeim2021@","foeimorg_combine");*/
+
+?>
+
+<?php
+$mysqli = mysqli_connect("localhost","foeimorg","foeim2021@","foeimorg_combine");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+/* check if server is alive */
+if ($mysqli->ping()) {
+    printf ("Our connection is ok!\n");
+} else {
+    printf ("Error: %s\n", $mysqli->error);
+}
+
+/* close connection */
+$mysqli->close();
+
+?>
