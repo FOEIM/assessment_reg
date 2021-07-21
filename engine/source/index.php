ee4345b824ea704b6c6289ac5b36b3a1ad11f2b0
FOEIM <87746026+FOEIM@users.noreply.github.com>
2021-07-21 12:04:40 +0630

    Create index.php

---
+++ engine/source/index.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(1);
+$mysqli = mysqli_connect("localhost","foeimorg","foeim2021@","foeimorg_combine");
+
+$email = $_GET['email'];
+
+$foeim = "SELECT * FROM assessment_data WHERE user_email ='$email'";
+        $rs = mysqli_query($mysqli ,$foeim);
+        $numUsers = mysqli_num_rows($rs);
+
+        if($numUsers > 0) {
+            header('location: https://assessment.foeim.org?err=<font color="red">Alredy Regiserterd and you cannot do this again</font>');
+        }
+else
+{
+
+      $foeim_query = "INSERT INTO assessment_data(user_email) VALUES ('$email')";
+        if ($mysqli->query($foeim_query) === TRUE)
+        {
+            header('location: https://facebook.com/minnyi158');
+        }
+        else
+        {
+            echo "Something Went Wrong";
+        }
+}
+?>
